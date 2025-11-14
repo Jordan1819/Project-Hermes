86c0cb695a714343bb4b35fc5b6ba7d5be4e5234
Jordan1819 <jmanwaite@gmail.com>
2025-11-14 01:06:39 -0700

    Updating project architecture

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let btnSave = document.getElementById('btnSave');
 let textArea = document.getElementById('mainTextArea');
 const WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbz4g9RQM-5JpeNXjnsrLwjMfoOuPE6nSA7GyszeRuTC3dtZjhmV2EXr9NNr63Fj8iZTmA/exec';
 
-// Clear button functionality
+// Clear button functionality - clear text area
 btnClear.addEventListener('click', function() {
     textArea.value = '';
 });
@@ -14,11 +14,13 @@ btnSave.addEventListener('click', function() {
     const text = textArea.value.trim();
     if (!text) return alert('Text area is empty!');
 
+    // Send text to google doc web app endpoint
     fetch(WEBAPP_URL, {
         method: 'POST',
         mode: 'no-cors',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text: text })
     });
+    // Clear text area after saving 
     textArea.value = '';
 });
